6ebf7da379d67f9344b24c62eb84c2153c142440
FerChava22 <fernandototal22@gmail.com>
2025-08-03 21:08:26 -0600

    Delete Logout.js

--- Logout.js
+++
@@ -1,9 +0,0 @@
-// logout.js
-
-function logout() {
-  // Borra los datos de sesión del usuario
-  localStorage.removeItem("usuarioSesion");
-
-  // Redirige a la página de login
-  window.location.href = "login.html";
-}
